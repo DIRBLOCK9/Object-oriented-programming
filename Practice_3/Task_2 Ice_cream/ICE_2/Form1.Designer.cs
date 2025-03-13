56cece026d81835e71d3676bafdac87e701520b9
Kostya <turlokosta7@gmail.com>
2025-03-14 00:00:32 +0200

    Practice_3
    Практична №3, завдання_21

---
+++ Practice_3/Task_2 Ice_cream/ICE_2/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace ICE_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rbChocolate = new System.Windows.Forms.RadioButton();
+            this.rbStrawberry = new System.Windows.Forms.RadioButton();
+            this.rbVanilla = new System.Windows.Forms.RadioButton();
+            this.rbBanana = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nudQuantity = new System.Windows.Forms.NumericUpDown();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nudQuantity)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rbChocolate
+            // 
+            this.rbChocolate.AutoSize = true;
+            this.rbChocolate.Location = new System.Drawing.Point(12, 21);
+            this.rbChocolate.Name = "rbChocolate";
+            this.rbChocolate.Size = new System.Drawing.Size(82, 17);
+            this.rbChocolate.TabIndex = 0;
+            this.rbChocolate.TabStop = true;
+            this.rbChocolate.Text = "Шоколадне";
+            this.rbChocolate.UseVisualStyleBackColor = true;
+            // 
+            // rbStrawberry
+            // 
+            this.rbStrawberry.AutoSize = true;
+            this.rbStrawberry.Location = new System.Drawing.Point(12, 44);
+            this.rbStrawberry.Name = "rbStrawberry";
+            this.rbStrawberry.Size = new System.Drawing.Size(79, 17);
+            this.rbStrawberry.TabIndex = 1;
+            this.rbStrawberry.TabStop = true;
+            this.rbStrawberry.Text = "Полуничне";
+            this.rbStrawberry.UseVisualStyleBackColor = true;
+            // 
+            // rbVanilla
+            // 
+            this.rbVanilla.AutoSize = true;
+            this.rbVanilla.Location = new System.Drawing.Point(12, 67);
+            this.rbVanilla.Name = "rbVanilla";
+            this.rbVanilla.Size = new System.Drawing.Size(70, 17);
+            this.rbVanilla.TabIndex = 2;
+            this.rbVanilla.TabStop = true;
+            this.rbVanilla.Text = "Ванільне";
+            this.rbVanilla.UseVisualStyleBackColor = true;
+            // 
+            // rbBanana
+            // 
+            this.rbBanana.AutoSize = true;
+            this.rbBanana.Location = new System.Drawing.Point(12, 90);
+            this.rbBanana.Name = "rbBanana";
+            this.rbBanana.Size = new System.Drawing.Size(74, 17);
+            this.rbBanana.TabIndex = 3;
+            this.rbBanana.TabStop = true;
+            this.rbBanana.Text = "Бананове";
+            this.rbBanana.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(174, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Кількість порцій";
+            // 
+            // nudQuantity
+            // 
+            this.nudQuantity.Location = new System.Drawing.Point(158, 68);
+            this.nudQuantity.Name = "nudQuantity";
+            this.nudQuantity.Size = new System.Drawing.Size(120, 20);
+            this.nudQuantity.TabIndex = 5;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(308, 67);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(100, 90);
+            this.txtResult.TabIndex = 6;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(34, 187);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(102, 41);
+            this.btnSubmit.TabIndex = 7;
+            this.btnSubmit.Text = "Підтвердити";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(100, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "40 грн";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(100, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "45 грн";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(100, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "35 грн";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(100, 90);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "50 грн";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 268);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.nudQuantity);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rbBanana);
+            this.Controls.Add(this.rbVanilla);
+            this.Controls.Add(this.rbStrawberry);
+            this.Controls.Add(this.rbChocolate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.nudQuantity)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rbChocolate;
+        private System.Windows.Forms.RadioButton rbStrawberry;
+        private System.Windows.Forms.RadioButton rbVanilla;
+        private System.Windows.Forms.RadioButton rbBanana;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown nudQuantity;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Practice_3/Task_2 Ice_cream/ICE_2/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ICE_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string selectedIceCream = string.Empty;
+
+            if (rbChocolate.Checked)
+                selectedIceCream = "Шоколадне";
+            else if (rbVanilla.Checked)
+                selectedIceCream = "Ванільне";
+            else if (rbStrawberry.Checked)
+                selectedIceCream = "Полуничне";
+            else if (rbBanana.Checked)
+                selectedIceCream = "Бананове";
+            else
+            {
+                MessageBox.Show("Будь ласка, виберіть сорт морозива.", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            
+            int quantity = (int)nudQuantity.Value;
+
+           
+            decimal pricePerPortion = 0;
+
+            switch (selectedIceCream)
+            {
+                case "Шоколадне":
+                    pricePerPortion = 40; 
+                    break;
+                case "Ванільне":
+                    pricePerPortion = 35;
+                    break;
+                case "Полуничне":
+                    pricePerPortion = 45;
+                    break;
+                case "Бананове":
+                    pricePerPortion = 50;
+                    break;
+            }
+
+           
+            decimal totalPrice = pricePerPortion * quantity;
+
+            
+            if (quantity > 20)
+            {
+                totalPrice *= 0.95m; 
+            }
+
+            
+            txtResult.Text = $"Ви замовили {quantity} порцію {selectedIceCream} морозива.\n" +
+                             $"Загальна вартість: {totalPrice} грн";
+        }
+    }
+    }
+
