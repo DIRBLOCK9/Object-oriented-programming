7233490f7cb9d56595fbb7a1efbd28e5f452a655
Kostya <turlokosta7@gmail.com>
2025-05-14 02:36:15 +0300

    Lab10
    Lab10

---
+++ Models/Animal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7.Models
+{
+    public class Animal
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Species { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+    }
+
+}
