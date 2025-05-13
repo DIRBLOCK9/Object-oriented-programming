260329f5d1581a051193efc71d79ce8cc3d6aa79
Kostya <turlokosta7@gmail.com>
2025-05-14 02:00:59 +0300

    Delete Models directory

--- Models/Animal.cs
+++
@@ -1,8 +0,0 @@
-namespace PetStoreApp.Models
-{
-    public class Animal
-    {
-        public string Name { get; set; } = string.Empty;
-        public string Species { get; set; } = string.Empty;
-    }
-}
